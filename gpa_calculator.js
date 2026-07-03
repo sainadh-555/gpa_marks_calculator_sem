20929fa471d4a8b602e94029e3f59dc2aef8e821
sainadh-555 <snadh843@gmail.com>
2026-07-03 11:19:49 +0530

    Fix final grade calculation in GPA calculator

--- gpa_calculator.js
+++ gpa_calculator.js
@@ -47,7 +47,7 @@ function calculateGPA() {
     let cs_grade = (cs_internal + cs_lab)/10;
     let code_grade = (code_internal + (code_sem * 40/60))/10;
 
-    let final_grade = (4*(ep_grade + code_grade)) + (3*(psup_grade + es_grade + egd_grade)) + (2*(tec_grade)) + (1*(cs_grade))/20;
+    let final_grade = ((4*(ep_grade + code_grade)) + (3*(psup_grade + es_grade + egd_grade)) + (2*(tec_grade)) + (1*(cs_grade)))/20;
 
     // Display results, formatting to 2 decimal places for neatness
     document.getElementById('res_psup').innerText = psup_grade.toFixed(2);
